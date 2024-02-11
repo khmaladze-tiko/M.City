ad5a40da6426c07b8f17f86970ea7b0a2bb2df89
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-02-11 04:45:58 +0400

    The sign in form

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Header from "./Components/Header_footer/header";
 import Footer from "./Components/Header_footer/footer";
 import Home from "./Components/Home";
+import SignIn from "./Components/Signin";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Header/>
       <Switch>
+        <Route path="/sign_in" exact component={SignIn} /> 
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer/>
