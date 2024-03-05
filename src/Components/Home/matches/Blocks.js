296852b55a38a16767a7e8a5ec0a7d3575178ea8
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-03-06 00:27:08 +0400

    Home matches part two

--- src/Components/Home/matches/Blocks.js
+++ src/Components/Home/matches/Blocks.js
@@ -2,43 +2,58 @@ import React, { useState, useEffect } from "react";
 // import { Slide } from "react-toastify";
 import { Slide } from "react-awesome-reveal";
 import { matchesCollection } from "../../../firebase"
+import MatchesBlock from "../../Utils/matches_block";
+
 
 
 const Blocks = () => {
-    const [lastVisible, setLastVisible] = useState(null);
-    const [matches, setMatches] = useState(null);
+    // const [lastVisible, setLastVisible] = useState(null);
+    const [matches, setMatches] = useState([]);
 
     useEffect(() => {
-            if(!matches) {
+            if(!matches.lenght > 0) {
                 matchesCollection.get().then(snapshot => {
-                    const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+                    // const lastVisible = snapshot.docs[snapshot.docs.length - 1];
                     const matches = snapshot.docs.map(doc => ({
                         id: doc.id,
                         ...doc.data()
                     }))
         
-                    setLastVisible(lastVisible);
+                    // setLastVisible(lastVisible);
                     setMatches(matches);
         
         
                 }).catch(error => {
                     // showToastError(error)
                     console.log(error);
-                }).finally(() => {
-                    // setLoading(false);
-                    console.log('final');
                 })
+                // .finally(() => {
+                //     // setLoading(false);
+                //     console.log('final');
+                // })
             }
-      }, [matches])
+      }, [matches]);
 
-      console.log(matches);
   
-    
+    const showMatches = (matches) => (
+        matches ? 
+            matches.map((match) => (
+                <Slide bottom key={match.id} className="item" triggerOnce>
+                    <div>
+                        <div className="wrapper">
+                            <MatchesBlock match={match}/>
+                        </div>
+                    </div>
+                </Slide>
+            ))
+        :
+        null
+    )
 
 
     return(
-        <div>
-            hello
+        <div className="home_matches">
+            {showMatches(matches)}
         </div>
     )
 
