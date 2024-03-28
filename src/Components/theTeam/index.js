91a9890681c3c6e568652f63659d7f74fda88577
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-03-29 02:59:23 +0400

    Creating 'the team' section

---
+++ src/Components/theTeam/index.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import PlayerCard from '../Utils/playerCard';
+import { Slide } from "react-awesome-reveal";
+import { Promise } from 'core-js';
+
+import { showErrorToast, showSuccessToast } from '../Utils/tools';
+import { firebase, playersCollection } from '../../firebase';
+
+
+const TheTeam = () => {
+    const [loading, setLoading] = useState(true);
+    const [players, setPlayers] = useState(null);
+
+    useEffect(() => {
+        if(!players){
+            playersCollection
+            .get()
+            .then( snapshot => {
+                const players = snapshot.docs.map( doc => ({
+                    id: doc.id,...doc.data()
+                }))
+                let promises = [];
+
+                    players.forEach((player,index) => {
+                        promises.push(
+                            new Promise((resolve,reject) => {
+                                firebase.storage().ref('players')
+                                .child(player.image).getDownloadURL()
+                                .then((url) => {
+                                    players[index].url = url;
+                                    resolve()
+                                }).catch((error)=>{
+                                    reject()
+                                });
+                            }),
+                        )
+                    })
+                    Promise.all(promises).then(() => {
+                        setPlayers(players)
+                    })
+
+            }).catch(errors => {
+                showErrorToast('Sorry try again later')
+            }).finally(() => {
+                setLoading(false)
+            })
+        }
+    },[players])
+
+    console.log(players)
+
+
+    return(
+        <div>
+            the team
+        </div>
+    )
+}
+
+export default TheTeam;
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Header from "./Components/Header_footer/header";
 import Footer from "./Components/Header_footer/footer";
 import Home from "./Components/Home";
 import SignIn from "./Components/Signin";
+import TheTeam from "./Components/theTeam";
 
 import Dashboard from "./Components/Admin/Dashboard";
 import AdminPlayers from "./Components/Admin/players"
@@ -21,6 +22,7 @@ const Routes = ({user}) => {
         <Route path="/admin_players/add_player" exact component={AuthGuard(AddEditPlayers)} />
         <Route path="/admin_players" exact component={AuthGuard(AdminPlayers)} />
         <Route path="/dashboard" exact component={AuthGuard(Dashboard)} />
+        <Route path="/the_team" component={AuthGuard(TheTeam)} />
         <Route 
           path="/sign_in" 
           exact 
