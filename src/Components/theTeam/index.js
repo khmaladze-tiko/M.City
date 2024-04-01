5a92e43522dec40d6503b32e727019496b40d99f
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-04-02 03:41:45 +0400

    some changes in team list

--- src/Components/theTeam/index.js
+++ src/Components/theTeam/index.js
@@ -7,114 +7,89 @@ import { showErrorToast } from "../Utils/tools";
 import { CircularProgress } from "@mui/material";
 import { firebase, playersCollection } from "../../firebase";
 
-
 const TheTeam = () => {
-    const [loading, setLoading] = useState(true);
-    const [players, setPlayers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [players, setPlayers] = useState(null);
 
-    useEffect(() => {
-        if(!players){
-            playersCollection
-            .get()
-            .then( (snapshot) => {
-                const players = snapshot.docs.map( doc => ({
-                    id: doc.id,
-                    ...doc.data()
-                }))
-                let promises = [];
+  useEffect(() => {
+    if (!players) {
+      playersCollection
+        .get()
+        .then((snapshot) => {
+          const players = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          let promises = [];
 
-                    players.forEach((player,index) => {
-                        promises.push(
-                            new Promise((resolve,reject) => {
-                                firebase
-                                .storage()
-                                .ref("players")
-                                .child(player.image)
-                                .getDownloadURL()
-                                .then((url) => {
-                                    players[index].url = url;
-                                    resolve()
-                                })
-                                    .catch((error)=>{
-                                    reject()
-                                });
-                            }),
-                        )
-                    })
-                    Promise.all(promises).then(() => {
-                        setPlayers(players)
-                    })
-
-            }).catch((errors) => {
-                showErrorToast("Sorry try again later")
-            }).finally(() => {
-                setLoading(false)
-            })
-        }
-    },[players])
-
-    const showPlayerByCategory = (category) => (
-        players ?
-            players.map((player, i) => {
-                return player.position === category ?
-                <Slide left key={player.id} triggerOnce>
-                    <div className="item">
-                        <PlayerCard
-                            number={player.number}
-                            name={player.name}
-                            lastname={player.lastname}
-                            bck={player.url}
-                        />
-                    </div>
-                </Slide>
-            :null
+          players.forEach((player, index) => {
+            promises.push(
+              new Promise((resolve, reject) => {
+                players[index].url =
+                  "https://tse2.mm.bing.net/th?id=OIP.8UIpb4qdQY_KzU5I8uoligHaEo&pid=Api&P=0&h=220";
+                resolve();
+              })
+            );
+          });
+          Promise.all(promises).then(() => {
+            setPlayers(players);
+          });
         })
-        :null
-    )
+        .catch((errors) => {
+          showErrorToast("Sorry try again later");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [players]);
 
-    return(
-        <div className="the_team_container">
-            { loading ? (
-                <div className="progress">
-                    <CircularProgress/>
-                </div>
-            )
-                :
-            (
-                <div>
-                    <div className="team_category_wrapper">
-                        <div className="title">Keepers</div>
-                        <div className="team_cards">
+  const showPlayerByCategory = (category) =>
+    players
+      ? players.map((player, i) => {
+          return player.position === category ? (
+            <Slide left key={player.id} triggerOnce>
+              <div className="item">
+                <PlayerCard
+                  number={player.number}
+                  name={player.name}
+                  lastname={player.lastname}
+                  bck={player.url}
+                />
+              </div>
+            </Slide>
+          ) : null;
+        })
+      : null;
 
-                            {showPlayerByCategory("Keeper")}
-                        </div>
-                    </div>
-                    <div className="team_category_wrapper">
-                        <div className="title">Defence</div>
-                        <div className="team_cards">
-
-                            {showPlayerByCategory("Defence")}
-                        </div>
-                    </div>
-                    <div className="team_category_wrapper">
-                        <div className="title">Midfield</div>
-                        <div className="team_cards">
-
-                            {showPlayerByCategory("Midfield")}
-                        </div>
-                    </div>
-                    <div className="team_category_wrapper">
-                        <div className="title">Strikers</div>
-                        <div className="team_cards">
-
-                            {showPlayerByCategory("Striker")}
-                        </div>
-                    </div>
-                </div>
-            )}
+  return (
+    <div className="the_team_container">
+      {loading ? (
+        <div className="progress">
+          <CircularProgress />
         </div>
-    )
-}
+      ) : (
+        <div>
+          <div className="team_category_wrapper">
+            <div className="title">Keepers</div>
+            <div className="team_cards">{showPlayerByCategory("Keeper")}</div>
+          </div>
+          <div className="team_category_wrapper">
+            <div className="title">Defence</div>
+            <div className="team_cards">{showPlayerByCategory("Defence")}</div>
+          </div>
+          <div className="team_category_wrapper">
+            <div className="title">Midfield</div>
+            <div className="team_cards">{showPlayerByCategory("Midfield")}</div>
+          </div>
+          <div className="team_category_wrapper">
+            <div className="title">Strikers</div>
+            <div className="team_cards">{showPlayerByCategory("Striker")}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default TheTeam;
-
